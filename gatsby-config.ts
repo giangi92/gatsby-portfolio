3be98a1efe9cc7cca48acd13b14ed774bb00dd17
gianluca <gianluca.lingua@txtgroup.com>
2022-04-17 22:06:31 +0200

    First commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,22 +2,58 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `Portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
+    // Used for the title template on pages other than the index site
+    siteTitle: `Portfolio`,
+    // Default title of the page
+    siteTitleAlt: `Portfolio - @giangisoft`,
+    // Can be used for e.g. JSONLD
+    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
+    // Will be used to generate absolute URLs for og:image etc.
+    siteUrl: `https://cara.lekoarts.de`,
+    // Used for SEO
+    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    // Will be set on the <html /> tag
+    siteLanguage: `en`,
+    // Used for og:image and must be placed inside the `static` folder
+    siteImage: `/banner.jpg`,
+    // Twitter Handle
+    author: `@giangisoft`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-cara`,
+      options: {
+        // basePath defaults to `/`
+        basePath: `/`,
+      },
     },
-    __key: "images"
-  }]
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `sections`,
+        path: `./src/@lekoarts/gatsby-theme-cara/sections`,
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
 };
 
 export default config;
---
+++ src/pages/testpage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const TestComponent = () => {
+    return <>
+    <h3>Componente di test importato in index</h3>
+    <p>Mostro un semplice contenuto <br /> e vado accapo</p>
+    </>
+}
